6541294be2023886b749f573cb7280f1c01e5c77
Luís Santos <80392073+TsLu1s@users.noreply.github.com>
2023-02-16 16:53:38 +0000

    Update atlantic.py

--- src/atlantic/atlantic.py
+++ src/atlantic/atlantic.py
@@ -920,31 +920,31 @@ def vif_performance_selection(train:pd.DataFrame,
    
 ########################################################### Metrics ########################################################################
 
-def metrics_regression(y_real, y_pred): 
+def metrics_regression(y_true, y_pred): 
     """
     Function to calculate various regression model evaluation metrics
     Parameters:
-    y_real: array-like, shape = (n_samples)
+    y_true: array-like, shape = (n_samples)
             Real values of the target
     y_pred: array-like, shape = (n_samples)
             Predicted values of the target
     Returns:
     Metrics_Prev_Regression: dictionary with keys as the metrics names, and values as the respective values
     """
-    mae=mean_absolute_error(y_real, y_pred)
-    mape= mean_absolute_percentage_error(y_real, y_pred)
-    mse=mean_squared_error(y_real, y_pred)
-    evs= explained_variance_score(y_real, y_pred)
-    maximo_error= max_error(y_real, y_pred)
-    r2=r2_score(y_real, y_pred)
-    Metrics_Prev_Regression= {'Mean Absolute Error': mae, 
+    mae=mean_absolute_error(y_true, y_pred)
+    mape= mean_absolute_percentage_error(y_true, y_pred)
+    mse=mean_squared_error(y_true, y_pred)
+    evs= explained_variance_score(y_true, y_pred)
+    maximo_error= max_error(y_true, y_pred)
+    r2=r2_score(y_true, y_pred)
+    metrics_pred_regression= {'Mean Absolute Error': mae, 
                               'Mean Absolute Percentage Error': mape,
                               'Mean Squared Error': mse,
                               'Explained Variance Score': evs, 
                               'Max Error': maximo_error,
                               'R2 Score':r2}  
     
-    return Metrics_Prev_Regression
+    return metrics_pred_regression
 
 def metrics_classification(y_true, y_pred):
 
@@ -952,13 +952,13 @@ def metrics_classification(y_true, y_pred):
     precision_metric = precision_score(y_true, y_pred,average='micro')
     f1_macro_metric = f1_score(y_true, y_pred,average='macro')
     
-    Metrics_Pred_Classification= {
+    metrics_pred_classification= {
                               'Accuracy': accuracy_metric,
                               'Precision Micro': precision_metric,
                               'F1 Score Macro':f1_macro_metric,
                               }
     
-    return Metrics_Pred_Classification
+    return metrics_pred_classification
 
 def metrics_binary_classification(y_true, y_pred):
     
@@ -969,7 +969,7 @@ def metrics_binary_classification(y_true, y_pred):
     average_precision_metric = average_precision(y_true, y_pred)
     balanced_accuracy_metric = balanced_accuracy(y_true, y_pred)
     
-    Metrics_Pred_Classification= {'Accuracy': accuracy_metric, 
+    metrics_pred_classification= {'Accuracy': accuracy_metric, 
                               'Precision Score': precision_metric,
                               'F1 Score': f1_metric,
                               'Recall Score': recall_metric,
@@ -977,7 +977,7 @@ def metrics_binary_classification(y_true, y_pred):
                               'Balanced Accuracy': balanced_accuracy_metric
                               }
 
-    return Metrics_Pred_Classification
+    return metrics_pred_classification
 
 ###########################################################  Tree Based Algorithms   #######################################################
 
