4ef2d317ccb4d93f09e6a5a8e2ddd7e267263566
Luís Santos <80392073+TsLu1s@users.noreply.github.com>
2023-04-17 14:29:32 +0100

    Update custom_preprocessing.py

--- examples/custom_preprocessing.py
+++ examples/custom_preprocessing.py
@@ -5,7 +5,6 @@ from sklearn.model_selection import train_test_split
 import warnings
 warnings.filterwarnings("ignore", category=Warning) #-> For a clean console
 
-#source_data="https://www.kaggle.com/datasets/surekharamireddy/fraudulent-claim-on-cars-physical-damage"
 
 url="https://raw.githubusercontent.com/TsLu1s/Atlantic/main/data/Fraudulent_Claim_Cars_class.csv"
 
@@ -23,6 +22,8 @@ data.isna().sum()
 
 train,test = train_test_split(data, train_size=0.8)
 
+#train_,test_=train.reset_index(drop=True), test.reset_index(drop=True)
+
 #################################### ATLANTIC DATA PREPROCESSING #####################################
 
 ########################################################################
@@ -37,60 +38,60 @@ test = atl.engin_date(test,drop=True)
 ## Option 1: MultiColumn LabelEncoder
 
 le_fit=atl.fit_Label_Encoding(train,target)  
-train=atl.transform_Label_Encoding(train,le_fit)
-test=atl.transform_Label_Encoding(test,le_fit)
+train_le=atl.transform_Label_Encoding(train,le_fit)
+test_le=atl.transform_Label_Encoding(test,le_fit)
     
 ## Option 2: MultiColumn OneHotEncoder
 
 ohe_fit=atl.fit_OneHot_Encoding(train,target,n_distinct=10)
-train=atl.transform_OneHot_Encoding(train,ohe_fit)
-test=atl.transform_OneHot_Encoding(test,ohe_fit)
+train_2=atl.transform_OneHot_Encoding(train,ohe_fit)
+test_2=atl.transform_OneHot_Encoding(test,ohe_fit)
     
 ## Option 3: MultiColumn IDF
 
 idf_fit=atl.fit_IDF_Encoding(train,target)
-train=atl.transform_IDF_Encoding(train,idf_fit)
-test=atl.transform_IDF_Encoding(test,idf_fit)
+train_3=atl.transform_IDF_Encoding(train,idf_fit)
+test_3=atl.transform_IDF_Encoding(test,idf_fit)
 
 ########################################################################
 ################# Automated (Sklearn) Null Imputation [Only numeric features]
 
 ## Option 1: Simple Imputer
-imputer=atl.fit_SimpleImp(train,
+imputer=atl.fit_SimpleImp(train_le,
                           target=target,
                           strat='mean')
 
-train_simple=atl.transform_SimpleImp(train,
+train_simple=atl.transform_SimpleImp(train_le,
                                      target=target,
                                      imputer=imputer)
 
-test_simple=atl.transform_SimpleImp(test,
+test_simple=atl.transform_SimpleImp(test_le,
                                     target=target,
                                     imputer=imputer)
 
 ## Option 2: KNN Imputer
-imputer_knn=atl.fit_KnnImp(train,
+imputer_knn=atl.fit_KnnImp(train_le,
                            target=target,
                            neighbors=5)
 
-train_knn=atl.transform_KnnImp(train,
+train_knn=atl.transform_KnnImp(train_le,
                                target=target,
                                imputer=imputer_knn)
 
-test_knn=atl.transform_KnnImp(test,
+test_knn=atl.transform_KnnImp(test_le,
                               target=target,
                               imputer=imputer_knn)
 
 ## Option 3: Iterative Imputer
-imputer_iter=atl.fit_IterImp(train,
+imputer_iter=atl.fit_IterImp(train_le,
                              target=target,
                              order='ascending')
 
-train_iter=atl.transform_IterImp(train,
+train_iter=atl.transform_IterImp(train_le,
                                  target=target,
                                  imputer=imputer_iter)
 
-test_iter=atl.transform_IterImp(test,
+test_iter=atl.transform_IterImp(test_le,
                                 target=target,
                                 imputer=imputer_iter)
 
@@ -100,15 +101,13 @@ test_iter=atl.transform_IterImp(test,
 
 
 ## Option 1: H2O AutoML Feature Selection
-sel_columns, h2o_imp = atl.feature_selection_h2o(Dataset=train_iter,       # Dataset:pd.DataFrame ,target:str="Target_Column",
+sel_columns, h2o_imp = atl.feature_selection_h2o(dataset=data,       # dataset:pd.DataFrame ,target:str="Target_Column",
                                                  target=target,      #  total_vi:float [0.5,1], h2o_fs_models:int [1,50], encoding_fs:bool=True/False
                                                  total_vi=0.98,     
                                                  h2o_fs_models =7,
                                                  encoding_fs=True)
 
 ## Option 2: Variance Importance Factor Feature Selection
-sel_columns, vif_imp = atl.feature_selection_vif(Dataset=train_iter,   # Dataset:pd.DataFrame, target:str="Target_Column",
+sel_columns, vif_imp = atl.feature_selection_vif(dataset=train_iter,   # dataset:pd.DataFrame, target:str="Target_Column",
                                                  target=target,  # VIF:float [3,30]
                                                  VIF=10.0)
-
-
