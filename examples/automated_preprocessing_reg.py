bf52b715baa04403d2e2375f1ae650e58ca10022
Luís Santos <80392073+TsLu1s@users.noreply.github.com>
2024-05-26 22:59:27 +0100

    Update automated_preprocessing_reg.py

--- examples/automated_preprocessing_reg.py
+++ examples/automated_preprocessing_reg.py
@@ -1,6 +1,6 @@
 ######################################## REGRESSION ########################################
 
-import atlantic.pipeline as Atlantic
+from atlantic.pipeline import Atlantic
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
